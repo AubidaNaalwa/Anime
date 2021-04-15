20dc8ce9180ace4faf19ce6aa0efd2eac3d99591
Aubida Naalwa <aubida-naalwa@outlook.com>
2021-04-15 04:34:54 +0300

    Adding files to server

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,19 +1,40 @@
 
 import { observer, inject } from 'mobx-react'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Home.css'
+
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 function Home(props) {
     const [searchVlaue, setSearchValue] = useState('')
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(()=>{
+            setLoading(false)
+        },3000)
+    }, [])
 
     return (
         <div className="Home">
             {/* Arabic Manga Site
             Created By :Aubida Naalwa */}
-            <div className="Search" >
-                <input className="serachBar" type="text" value={searchVlaue} onChange={({target})=>setSearchValue(target.value)}/>
-                <button>Search</button>
-            </div>
-            
+            {
+                loading     
+             ?
+                        <Loader
+                            type="TailSpin"
+                            color="#00BFFF"
+                            height={100}
+                            width={100}
+                        />
+                        :
+                        <div className="Search" >
+                            <input className="serachBar" type="text" value={searchVlaue} onChange={({ target }) => setSearchValue(target.value)} />
+                            <button>Search</button>
+                        </div>
+            }
+
         </div>
 
     );
